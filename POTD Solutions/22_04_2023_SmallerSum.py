fdbab3b9e4da8e5c730bbf0fccb136a5324b0548
Neeraj <neerajantil.antil26@gmail.com>
2023-04-22 09:45:54 +0530

    Create 22_04_2023_SmallerSum.py

---
+++ POTD Solutions/22_04_2023_SmallerSum.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def smallerSum(self, n : int, arr : List[int]) -> List[int]:
+        cp = arr.copy()
+        cp.sort()
+        
+        dit = {}
+        pre_sum = 0
+        
+        for elem in cp:
+            if elem not in dit:
+                dit[elem] = pre_sum
+            
+            pre_sum += elem
+        
+        ans = [] 
+        for i in range(n):
+            ans.append(dit[arr[i]])
+            
+        return ans
