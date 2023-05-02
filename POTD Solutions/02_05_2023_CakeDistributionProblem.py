303b05c9b804c24276973d3fb5a76efe670b9f0a
Neeraj <neerajantil.antil26@gmail.com>
2023-05-02 18:15:03 +0530

    Create 02_05_2023_CakeDistributionProblem.py

---
+++ POTD Solutions/02_05_2023_CakeDistributionProblem.py
@@ -0,0 +1,32 @@
+class Solution():
+    def isPoss(self, sweetness, mid, k):
+        sum_val = 0
+        cnt = 0
+        for s in sweetness:
+            sum_val += s
+            if sum_val >= mid:
+                cnt += 1
+                sum_val = 0
+        return cnt >= k + 1    
+    
+    
+    def maxSweetness(self, sweetness, n, k):
+        sum_val = 0
+        min_sweetness = float('inf')
+        for s in sweetness:
+            sum_val += s
+            min_sweetness = min(min_sweetness, s)
+        
+        l = min_sweetness
+        h = sum_val
+        ans = 0
+        
+        while l <= h:
+            mid = (l + h) // 2
+            if self.isPoss(sweetness, mid, k):
+                ans = mid
+                l = mid + 1
+            else:
+                h = mid - 1
+        
+        return ans
