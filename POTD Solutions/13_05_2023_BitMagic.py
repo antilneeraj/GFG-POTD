7372d301de8d483c87dea3ec5fd47da9bea3e366
Neeraj <neerajantil.antil26@gmail.com>
2023-05-13 09:49:37 +0530

    Create 13_05_2023_BitMagic.py

---
+++ POTD Solutions/13_05_2023_BitMagic.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def bitMagic(self, n : int, arr : List[int]) -> int:
+        Operations = 0
+        i = 0
+        j = n - 1
+        while i < j:
+            if(arr[i] != arr[j]):
+                Operations += 1
+            i += 1
+            j -= 1
+        ans = str(Operations/2 + Operations%2)
+        return ans[:ans.index(".")] if "." in ans else ans
