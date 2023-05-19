d6376f86d9e1d670d5e659645977d54405769154
Neeraj <neerajantil.antil26@gmail.com>
2023-05-19 18:12:05 +0530

    Create 19_05_2023_kthSmallestElement.py

---
+++ POTD Solutions/19_05_2023_kthSmallestElement.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def kthSmallestNum(self, n: int, ranges: List[List[int]], q: int, queries: List[int]) -> List[int]:
+        ranges.sort()
+        ans = []
+        for query in queries:
+            last = -1
+            found = False
+            for i in range(n):
+                start, end = ranges[i]
+                if last < end and last >= start:
+                    t = end - last
+                    if t >= query:
+                        ans.append(last + query)
+                        found = True
+                        break
+                    last = end
+                    query -= t
+                elif last < start:
+                    t = end - start + 1
+                    if t >= query:
+                        ans.append(start + query - 1)
+                        found = True
+                        break
+                    last = end
+                    query -= t
+            if not found:
+                ans.append(-1)
+        return ans
