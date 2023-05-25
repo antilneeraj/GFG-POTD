fcfb2e872cf73807c5bbb1cf4aa18b98d56c0be4
Neeraj <neerajantil.antil26@gmail.com>
2023-05-25 17:36:10 +0530

    Create 25_05_2023_ExpressionAddOperators.py

---
+++ POTD Solutions/25_05_2023_ExpressionAddOperators.py
@@ -0,0 +1,24 @@
+class Solution:
+    def addOperators(self, S, target):
+        ans = []
+        if S is None or len(S) == 0:
+            return ans
+        self.helper(ans, "", S, target, 0, 0, 0)
+        return ans
+    
+    def helper(self, ans, path, num, target, ind, p_val, prev):    
+        if ind == len(num):
+            if target == p_val:
+                ans.append(path)
+            return
+    
+        for i in range(ind, len(num)):
+            cur = int(num[ind:i+1])
+            if ind == 0:
+                self.helper(ans, path + str(cur), num, target, i + 1, cur, cur)
+            else:
+                self.helper(ans, path + "+" + str(cur), num, target, i + 1, p_val + cur, cur)
+                self.helper(ans, path + "-" + str(cur), num, target, i + 1, p_val - cur, -cur)
+                self.helper(ans, path + "*" + str(cur), num, target, i + 1, p_val - prev + prev * cur, prev * cur)
+            if num[ind] == '0':
+                break
