aee41c339a545ea1c6fb3c29e4a492f194e33684
Neeraj <neerajantil.antil26@gmail.com>
2023-07-09 17:59:10 +0530

    Create 09_07_2023_SmallestPositiveMIssingNumber.py

---
+++ POTD Solutions/09_07_2023_SmallestPositiveMIssingNumber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self,arr,n):
+        for i in range(n):
+            while arr[i] > 0 and arr[i] <= n and arr[arr[i] - 1] != arr[i]:
+                arr[arr[i] - 1], arr[i] = arr[i], arr[arr[i] - 1]
+
+        for i in range(n):
+            if arr[i] != i + 1:
+                return i + 
+        return n + 1
