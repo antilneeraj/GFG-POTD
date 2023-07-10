d4163fec24576860a5aa7eb5b40ebb6fcbf934d2
Neeraj <neerajantil.antil26@gmail.com>
2023-07-10 10:43:49 +0530

    Create 10_07_2023_TransoseOfMatrix.py

---
+++ POTD Solutions/10_07_2023_TransoseOfMatrix.py
@@ -0,0 +1,5 @@
+class Solution:
+    def transpose(self, matrix, n):
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
